ecc1113903d260ac798fe84d1364bd9cef8f2de3
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-07-05 15:29:04 -0300

    guardando el js por separado

--- js/operacion.js
+++ js/operacion.js
@@ -1,13 +1,11 @@
-/*
-alert("Ingrese dos números para poder realizar una suma");
-numero1 = prompt('Primer número:','');
-numero2 = prompt('Segundo número:','');
-resultado = parseInt(numero1) + parseInt(numero2);
-alert("El resultado es " + resultado);
-console.log(resultado);
-*/
-
-
-/*function funcionSuma(valor1, valor2){
-  return parseInt(valor1) + parseint(valor2);
-}*/
+function funcionSuma(numero1, numero2){
+  let valor1 = document.getElementById("numero1").value;
+  let valor2 = document.getElementById("numero2").value;
+  let val1 = parseInt(valor1);
+  let val2 = parseInt(valor2);
+  let res = val1 + val2;
+  console.log(val1);
+  console.log(val2);
+  console.log(res);
+  document.getElementById("resultado").value = res;
+}
