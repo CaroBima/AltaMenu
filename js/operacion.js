270578697e73a9d76072c16bab9fa9a3fd065cc9
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-07-05 12:47:22 -0300

    agregando otro ejercicio

---
+++ js/operacion.js
@@ -0,0 +1,13 @@
+/*
+alert("Ingrese dos números para poder realizar una suma");
+numero1 = prompt('Primer número:','');
+numero2 = prompt('Segundo número:','');
+resultado = parseInt(numero1) + parseInt(numero2);
+alert("El resultado es " + resultado);
+console.log(resultado);
+*/
+
+
+/*function funcionSuma(valor1, valor2){
+  return parseInt(valor1) + parseint(valor2);
+}*/
